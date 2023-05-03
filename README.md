565162156fb4ffc46ca329ee47bc593463ea7035
Andrii Slabkyi <aslabkyi@gmail.com>
2023-05-03 16:53:19 +0300

    Complete

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# goit-node-hw-01
+1. Receive and display the entire list of contacts in the form of a table: https://ibb.co/GJzH71F;
+2. Receive a contact by id: https://ibb.co/xHy1Nhn;
+3. Add a contact: https://ibb.co/PDpDLc8;
+4. Delete the contact: https://ibb.co/8xBVN83;
---
+++ index.js
@@ -0,0 +1,39 @@
+const contacts = require("./contacts");
+
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+program.parse(process.argv);
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const contactById = await contacts.getContactById(id);
+      console.log(contactById);
+      break;
+
+    case "add":
+      const newContact = await contacts.addContact(name, email, phone);
+      console.log(newContact);
+      break;
+
+    case "remove":
+      const removeContact = await contacts.removeContact(id);
+      console.log("Your contact has been successfully deleted");
+      break;
+  }
+}
+
+invokeAction(options);
